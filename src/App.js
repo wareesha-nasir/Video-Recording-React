e6b1600a6f2f8d849ea08b22e120920814c78277
wareesha-nasir <warisha.nasir356@gmail.com>
2021-02-26 01:14:28 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import VideoRecorder from 'react-video-recorder';
+import {React,useState} from 'react';
+import {Dialog,DialogTitle,DialogContent,Button} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+  const [openCam,setOpen]=useState(false);
+
+return(
+  <div style={{textAlign:"center", marginTop:"10%",border:"1px solid grey", marginLeft:"30%",
+  marginRight:"30%",padding:"5%"}}>
+    <Button style={{backgroundColor:"red", fontSize:"25px",borderRadius:"5em"}} onClick={()=>setOpen(true)}>record</Button>
+  <Dialog open={openCam} fullWidth maxWidth="sm" >
+      <DialogTitle>
+          <div  >
+          Add new list
+          <Button onClick={()=>setOpen(false)} style={{float:"right"}}>X</Button></div>
+          </DialogTitle>
+      <DialogContent dividers>
+    
+      <VideoRecorder
+    onRecordingComplete={videoBlob => {
+      // Do something with the video...
+      console.log('videoBlob', videoBlob)
+    }}
+  />    
+      </DialogContent>
+  </Dialog>
+  </div>
+)
+
 }
 
 export default App;
